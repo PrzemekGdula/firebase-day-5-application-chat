785ef2e785fe4f28399f2e8371cfc5b075ca542f
Przemek Gdula <przemo.g12@wp.pl>
2019-04-12 10:54:04 +0200

    message from databse display

--- src/MessagesList.js
+++ src/MessagesList.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
-const MessagesList = (props) => (
-    <div>
+const MessagesList = (props) => {
+    const messagesArray = (
+        Object.entries(props.messages || {})
+            .map(
+                ([key, value]) => ({
+                    ...value,
+                    key,
+                })
+            )
+    )
 
-    </div>
-)
+    return (
+        <div>
+            {
+                messagesArray.map(
+                    message => (
+                        <div
+                            key={message.key}
+                        >
+                            {message.text}
+                        </div>
+                    )
+                )
+            }
+        </div>
+    )
+}
 
 export default MessagesList
