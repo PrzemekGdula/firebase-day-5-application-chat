5ebaabab0d509114725b574f2a38626a23ad6430
Przemek Gdula <przemo.g12@wp.pl>
2019-04-12 11:08:54 +0200

    icons added

--- src/Message.js
+++ src/Message.js
@@ -4,6 +4,11 @@ import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 import ListItemText from '@material-ui/core/ListItemText'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import IconButton from '@material-ui/core/IconButton'
+
+import StarBorderIcon from '@material-ui/icons/StarBorder'
+import StarIcon from '@material-ui/icons/Star'
 
 const Message = (props) => (
     <div>
@@ -20,6 +25,12 @@ const Message = (props) => (
                 primary={''}
                 secondary={''}
             />
+            <ListItemSecondaryAction>
+                <IconButton aria-label="Comments">
+                    <StarBorderIcon />
+                    <StarIcon />
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     </div>
 )
